3ae7d5dc29239439b54ee632a5f309e973876a37
yael <yael.atletl@gmail.com>
2020-10-26 00:38:31 -0600

    Añadidos botones de ruta

--- Main.gd
+++ Main.gd
@@ -1,10 +1,21 @@
 extends PanelContainer
 
+onready var confirm_dialog = $HSplitContainer/TabContainer/Tabs/VBoxContainer/Control/ConfirmationDialog
+onready var icon = preload("res://location_icon.png")
 func _ready():
-	pass # Replace with function body.
-
-
+	confirm_dialog.connect("request_new_point", self, "create_item")
 
 func _on_Punto_pressed():
-	$HSplitContainer/ScrollContainer/TabContainer/Tabs/VBoxContainer/Control/ConfirmationDialog.popup_centered()
-	pass # Replace with function body.
+	confirm_dialog.popup_centered()
+
+func create_item(point_info):
+	var panel = PanelContainer.new()
+	var box = HBoxContainer.new()
+	var texx = TextureButton.new()
+	var label = Label.new()
+	texx.texture_normal = icon
+	label.text = point_info.get("direccion")
+	panel.add_child(box)
+	box.add_child(texx)
+	box.add_child(label)
+	$HSplitContainer/TabContainer/Tabs/VBoxContainer/ScrollContainer/ItemList.add_child(panel)
