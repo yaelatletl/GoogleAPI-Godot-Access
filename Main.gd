b17ae5f794505e3067d9a908a40b38f419beef7e
yael <yael.atletl@gmail.com>
2020-10-26 00:03:30 -0600

    Añadido Formulario al Main

---
+++ Main.gd
@@ -0,0 +1,10 @@
+extends PanelContainer
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _on_Punto_pressed():
+	$HSplitContainer/ScrollContainer/TabContainer/Tabs/VBoxContainer/Control/ConfirmationDialog.popup_centered()
+	pass # Replace with function body.
